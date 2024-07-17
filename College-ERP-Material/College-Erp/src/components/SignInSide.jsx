85c9c190604cbc2543fb53670949aa7e7f9b051d
novice1205 <parthraheja1205@gmail.com>
2024-07-17 20:10:12 +0530

    Sign In Page

--- College-ERP-Material/College-Erp/src/components/SignInSide.jsx
+++ College-ERP-Material/College-Erp/src/components/SignInSide.jsx
@@ -5,13 +5,13 @@ import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
+// import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Link } from 'react-router-dom';
 
 function Copyright(props) {
   return (
@@ -106,8 +106,7 @@ export default function SignInSide() {
                 fullWidth
                 variant="contained"
                 sx={{ mt: 3, mb: 2 }}
-              >
-              Sign in
+              > <Link>Sign in</Link> 
               </Button>
               <Grid container>
                 <Grid item xs>
